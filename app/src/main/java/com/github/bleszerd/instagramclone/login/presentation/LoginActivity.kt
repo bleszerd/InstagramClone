7d2b8ee5918cb4fd027a88668d07380c37061be4
Vinicius Resende <alive2k@programmer.net>
2021-07-30 13:53:11 -0300

    EditInput error layout

--- app/src/main/java/com/github/bleszerd/instagramclone/login/presentation/LoginActivity.kt
+++ app/src/main/java/com/github/bleszerd/instagramclone/login/presentation/LoginActivity.kt
@@ -2,11 +2,22 @@ package com.github.bleszerd.instagramclone.login.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.github.bleszerd.instagramclone.R
+import com.github.bleszerd.instagramclone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val editLayoutEmail = binding.loginActivityInputLayoutEmail
+        editLayoutEmail.error = "Este e-mail é inválido!"
+
+        val editTextEmail = binding.loginActivityEditTextEmail
+        editTextEmail.background = ContextCompat.getDrawable(this, R.drawable.edit_text_background_error)
     }
 }
