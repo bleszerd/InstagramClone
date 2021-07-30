8b1b174ab62b65e8ad294976a1725be9c569fe22
Vinicius Resende <alive2k@programmer.net>
2021-07-30 10:23:52 -0300

    LoginActivity

---
+++ app/src/main/java/com/github/bleszerd/instagramclone/login/presentation/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.github.bleszerd.instagramclone.login.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.bleszerd.instagramclone.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
