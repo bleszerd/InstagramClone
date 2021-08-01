fd6777922c69aec9b363ed367fb1222032c6d38d
Vinicius Resende <alive2k@programmer.net>
2021-08-01 09:24:59 -0300

    Bottom navigation bar

---
+++ app/src/main/java/com/github/bleszerd/instagramclone/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.bleszerd.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.bleszerd.instagramclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
